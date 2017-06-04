c7bd9decf986346f733d234dbff604a2fe36daaa
Cédric Roux <cedroux42@gmail.com>
2017-06-04 15:03:21 +0200

    Moved helper to relevant directory

--- src/Commands/Overwatch/helpers.php
+++ src/Commands/Overwatch/helpers.php
@@ -1,25 +1,28 @@
 <?php
 
 if (! function_exists('get_rank')) {
+    /**
+     * Convert rank value to rank id
+     *
+     * @param int $rank
+     * @return int
+     */
     function get_rank(int $rank)
     {
-        $rank = 0;
         if ($rank < 1500) {
-            $rank = 1;
+            return 1;
         } elseif ($rank >= 1500 && $rank < 2000) {
-            $rank = 2;
+            return 2;
         } elseif ($rank >= 2000 && $rank < 2500) {
-            $rank = 3;
+            return 3;
         } elseif ($rank >= 2500 && $rank < 3000) {
-            $rank = 4;
+            return 4;
         } elseif ($rank >= 3000 && $rank < 3500) {
-            $rank = 5;
+            return 5;
         } elseif ($rank >= 3500 && $rank < 4000) {
-            $rank = 6;
+            return 6;
         } elseif ($rank >= 4000) {
-            $rank = 7;
+            return 7;
         }
-
-        return $rank;
     }
 }

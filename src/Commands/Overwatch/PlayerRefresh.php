5f0fc948f565d5bf72557bd9d5b94c3a4fa3e6bc
Cédric Roux <cedroux42@gmail.com>
2017-06-06 01:32:10 +0200

    Emojis & mentions in refresh

--- src/Commands/Overwatch/PlayerRefresh.php
+++ src/Commands/Overwatch/PlayerRefresh.php
@@ -16,18 +16,22 @@ class PlayerRefresh extends BaseCommand
     public function execute()
     {
         $players = Database::select();
+        $guild = $this->message->channel->guild->id;
 
         foreach ($players as $player) {
             echo '-- Updating ' . $player->battletag . '...' . PHP_EOL;
 
             $newRank = Parser::rank($player->battletag);
+            $icon = get_emoji($guild, ':rank' . get_rank($newRank) . ':');
             $tag = explode('#', $player->battletag)[0];
             $diff = $newRank - $player->rank;
 
+            $discord = ! empty($player->discord) ? "(<@!{$player->discord}>) " : '';
+
             if ($diff < 0) {
-                $this->broadcast(":x: {$tag} vient de perdre **" . abs($diff) . "** points. Nouveau classement : **{$newRank}**");
+                $this->broadcast(":x: {$tag} {$discord}vient de perdre **" . abs($diff) . "** points. Nouveau classement : {$icon} **{$newRank}**");
             } elseif ($newRank > $player->rank) {
-                $this->broadcast(":white_check_mark: {$tag} vient de gagner **{$diff}** points. Nouveau classement : **{$newRank}**");
+                $this->broadcast(":white_check_mark: {$tag} {$discord}vient de gagner **{$diff}** points. Nouveau classement : {$icon} **{$newRank}**");
             }
             $player->rank = $newRank;
             Database::update($player, 'battletag', $player->battletag);
